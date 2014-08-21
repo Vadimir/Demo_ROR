fd28b4e360eaa33a86ba2b8402d5fffd4407a784
Vadzim_Kruchkou <vadkruch@gmail.com>
2014-08-21 20:24:19 +0300

    Initial commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+rails console RAILS_ENV=production
 # Initialize the Rails application.
 Rails.application.initialize!
+development=false
+test=false
+production=true
